b70b3550c77107fa7444191737ad416c558da80b
Abeer Al-Nej <shahadhh1995@gmail.com>
2021-09-21 23:45:00 +0300

    My calculator

--- calculator.js
+++ calculator.js
@@ -2,5 +2,24 @@ const prompt = require("prompt-sync")(); // Please don't touch me :)
 
 // The prompt method allows us to take an input from the user.
 // Run the code to see how to use prompt.
-const name = prompt("What's your name? ");
-console.log(`Hi, I'm ${name}.`);
+//const name = prompt("What's your name? ");
+//console.log(`Hi, I'm ${name}.`);
+
+const firstnum=+prompt("Please enter the first number:")
+const secondnum=+prompt("Please enter the second number:")
+const operation=prompt("Please choose the operation(+,-,/,*):")
+
+if(!isNaN(firstnum) && !isNaN(secondnum)){
+
+   if(operation === "+"){
+    console.log(`the answer is ${firstnum + secondnum}`)
+} else if(operation === "-"){
+    console.log(`the answer is ${firstnum-secondnum}`)
+} else if(operation === "*"){
+    console.log(`the answer is ${firstnum*secondnum}`)
+} else if(operation === "/"){
+    console.log(`the answer is ${firstnum/secondnum}`)}
+    else { (console.log("operation is invalid"))
+}} 
+else {
+    console.log("Invalid numbers") }
